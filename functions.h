981f39146d058882a628ec9e403728ed48d4e48e
Joseph Piccoli <joe13676@comcast.net>
2020-10-04 15:30:23 -0700

    Implement code for radix sort
    Implement code for radix sort.

--- functions.h
+++ functions.h
@@ -2,7 +2,8 @@
 
 #include <iostream>
 #include <vector>
+#include <list>
 
 void counting_sort(std::vector<int> &vec);
-void radix_sort();
+void radix_sort(std::vector<int> &vec, int max_digits);
 void chessboard_squares();
--- radix_sort.cpp
+++ radix_sort.cpp
@@ -1,6 +1,39 @@
 #include "functions.h"
 
-void radix_sort()
+void radix_sort(std::vector<int> &vec, int max_digits)
 {
-  std::cout << "radix_sort()\n";
+  std::list<int> pockets[ 10 ];
+
+  for(int i = 0; i < max_digits; ++i)
+  {
+    // Store in each pocket each element that has a digit for that pocket
+    int m = static_cast<int>(pow(10, i + 1));
+    int p = static_cast<int>(pow(10, i));
+    std::cout << "m = " << m << " p = " << p << '\n';
+    for(unsigned j = 0; j < vec.size(); ++j)
+    {
+      int temp = vec[ j ] % m;
+      int index = temp / p;
+      pockets[ index ].push_back(vec[ j ]);
+    }
+    int count = 0;
+
+    for(int j = 0; j < 10; ++j)
+    {
+      // Debug
+      //std::cout << "Pocket j = " << j << ": ";
+      //for(auto const item : pockets[ j ])
+      //  std::cout << item << " ";
+      //std::cout << '\n';
+      // Debug
+
+      while(!pockets[ j ].empty())
+      {
+        vec[ count ] = *(pockets[ j ].begin());
+        //std::cout << "count = " << count << ", vec[ count ] = " << vec[ count ] << '\n';
+        pockets[ j ].erase(pockets[ j ].begin());
+        count++;
+      }
+    }
+  }
 }
